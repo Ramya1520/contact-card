3b8add80a24f341f60014cee623eb77cc59e3001
Ramya1520 <ramya.krishnan1520@gmail.com>
2022-12-26 14:02:21 +0530

    edit

--- src/components/Contacts/EditContact/EditContact.jsx
+++ src/components/Contacts/EditContact/EditContact.jsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import{Link} from 'react-router-dom';
 let EditContact=()=>{
     return(
-             <React.Fragment><h2>EditContact</h2></React.Fragment>
+        <React.Fragment>
+        <section className='add-contact p-3'>
+            <div className='container'>
+                <div className='row'>
+                    <div className='col'>
+                        <p className="fw-bold">Edit Contact</p>
+                        {/* <p className='fst-italic'>Edit Contact</p> */}
+                    </div>
+                </div>
+                <div className='row'>
+                    <div className='col-md-4'>
+                        <form>
+                            <div className='mb-2'>
+                                <input type="text"  className='form-control' placeholder='Name'></input>
+                            </div>
+                            <div className='mb-2'>
+                                <input type="text" className='form-control'  placeholder='Photo Url'></input>
+                            </div>
+                            <div className='mb-2'>
+                                <input type="number" className='form-control' placeholder='Mobile'></input>
+                            </div>
+                            <div className='mb-2'>
+                                <input type="email" className='form-control' placeholder='Email'></input>
+                            </div>
+                            <div className='mb-2'>
+                                <input type="text" className='form-control' placeholder='Company'></input>
+                            </div>
+                            <div className='mb-2'>
+                                <input type="text" className='form-control' placeholder='Title'></input>
+                            </div>
+                            <div>
+                            <select className='form-control'>
+                                <option value="">Select a Group</option>
+                            </select>
+                            </div>
+                            <div className='mb-2'>
+                                <input type="Submit" className='btn btn-primary ms-2' value="Update" ></input>
+                                <Link to={'/contacts/list'}className="btn btn-danger ms-2">Cancel</Link>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+            </div>
+
+        </section>
+        </React.Fragment>
     )
 };
 export default EditContact;
